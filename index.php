d4a4168d00bf7cb03d18a3ebda26f8fcc0cb4494
TamasJonhson <100958931+TamasJonhson@users.noreply.github.com>
2022-04-08 16:57:38 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -2,5 +2,7 @@
 
 require_once __DIR__  . '/vendor/autoload.php';
 require_once __DIR__  . '/includes/header.php';
-require_once __DIR__  . '/includes/TelaCadastro.php';
+require_once __DIR__  . '/includes/footer.php';
+require_once __DIR__  . '/includes/listagem.php';
+
  
